09ee7016cc6b228b7843ca1f97cbe7ed7d3b4f1f
Jake Schaefbauer <jschaefmn@gmail.com>
2022-05-18 20:53:28 -0500

    finished 3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,12 +5,13 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight = function () {
+
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -61,4 +62,12 @@ var fight = function () {
   }
 };
 
-fight();
+// for (var i = 0; i < enemyNames.length; i++) {
+//   console.log(enemyNames[i]);
+//   console.log(i);
+//   console.log(enemyNames[i] + " is at " + i + " index")
+// }
+
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
